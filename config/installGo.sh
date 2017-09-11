434dca36486c8b3da74f14e6e61e02fee0ef50ce
Greg Werner <werner.greg@gmail.com>
2017-09-11 13:39:30 -0400

    move install to script
    Signed-off-by: Greg Werner <werner.greg@gmail.com>

---
+++ config/installGo.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#!/bin/bash
+set -e
+
+VERSION="1.8.1"
+
+DFILE="go$VERSION.linux-amd64.tar.gz"
+
+if [ -d "$HOME/.go" ] || [ -d "$HOME/go" ]; then
+    echo "Installation directories already exist. Exiting."
+    exit 1
+fi
+
+echo "Downloading $DFILE ..."
+wget https://storage.googleapis.com/golang/$DFILE -O /tmp/go.tar.gz
+if [ $? -ne 0 ]; then
+    echo "Download failed! Exiting."
+    exit 1
+fi
+
+echo "Extracting ..."
+tar -C "$HOME" -xzf /tmp/go.tar.gz
+mv "$HOME/go" "$HOME/.go"
+
+echo "Export environment variables ..."
+GOROOT=$HOME/.go
+PATH=$PATH:$GOROOT/bin
+GOPATH=$HOME/go
+PATH=$PATH:$GOPATH/bin
+
+export $GOROOT
+export $PATH
+export $GOPATH
+export $PATH
+
+mkdir -p $HOME/go/{src,pkg,bin}
+echo -e "\nGo $VERSION was installed."
+rm -f /tmp/go.tar.gz
