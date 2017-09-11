d6d93f70cd7b07f0219299978084ee8ea3db5cd1
Greg Werner <werner.greg@gmail.com>
2017-09-11 16:12:47 -0400

    test builds

--- config/installGo.sh
+++ config/installGo.sh
@@ -3,14 +3,4 @@ set -e
 
 curl -sL -o $HOME/bin/gimme https://raw.githubusercontent.com/travis-ci/gimme/master/gimme
 chmod +x $HOME/bin/gimme
-echo "Updating gimme"
-GIMME_OUTPUT="$(gimme 1.9 | tee -a $HOME/.bashrc)" && eval "$GIMME_OUTPUT"
-export GOPATH=$HOME/gopath
-export PATH=$HOME/gopath/bin:$PATH
-mkdir -p $HOME/gopath/src/github.com/3Blades/core-server
-rsync -az ${TRAVIS_BUILD_DIR}/ $HOME/gopath/src/github.com/3Blades/core-server/
-export TRAVIS_BUILD_DIR=$HOME/gopath/src/github.com/3Blades/core-server
-cd $HOME/gopath/src/github.com/3Blades/core-server
-gimme version
-go version
-go env
+gimme stable
